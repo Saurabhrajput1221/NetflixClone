eaae9686afc02bc08ab8f55447b92b78643947d0
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-03 12:50:08 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import "./App.css";
+import Nav from "./Nav";
+import Banner from './Banner';
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
-  return <div className="app">Hello
-  <h1>king</h1></div>;
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <Banner></Banner>
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetFlixOriginals}
+        isLargeRow={true}
+      />
+
+      <Row title="Trending Now" fetchUrl={requests.fetchTreding} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanticMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
 }
 
 export default App;
